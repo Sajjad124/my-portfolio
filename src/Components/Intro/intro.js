b56bfb61d52aede1fe56a5e816a8a819192c36c1
Muhammad Umair <181370124@gift.edu.pk>
2022-08-19 18:19:51 +0500

    updated
    icons styling for linkedin, instagram and github

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -1,5 +1,9 @@
 import React from "react";
-import './intro.css';
+import "./intro.css";
+import Github from "../../img/github.png";
+import Linkedin from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+
 const intro = () => {
   return (
     <div className="intro">
@@ -8,17 +12,18 @@ const intro = () => {
           <span>Hi! I Am </span>
           <span>Sajjad</span>
           <span>
-            Reactjs Developer with
-             high levelof experience in web designing and
+            Reactjs Developer with high levelof experience in web designing and
             development, producting the quaility work
           </span>
         </div>
         <button className="button i-button">Hire me</button>
+        <div className="i-icons">
+          <img src={Github} alt=""></img>
+          <img src={Linkedin} alt=""></img>
+          <img src={Instagram} alt=""></img>
+        </div>
       </div>
-      <div className="i-right">
-        i am right side 
-      </div>
-      
+      <div className="i-right">i am right side</div>
     </div>
   );
 };
