9c89ec0617eed19b6e0ef3666d8c5367cbe13af7
Muhammad Umair <181370124@gift.edu.pk>
2022-08-20 23:56:55 +0500

    Update intro.js

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -41,8 +41,8 @@ const intro = () => {
         <img src={Vector1} alt=""></img>
         <img src={Vector2} alt=""></img>
         <img
-          src={Sajjad2}
-          style={{ width: "300px", height: "400px"}}
+          src={Boy}
+          // style={{ width: "300px", height: "400px"}}
           alt=""
         ></img>
         <div>
