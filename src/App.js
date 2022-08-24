60c4d5430f240f323f61f85e5b400b44f2b4e44e
Muhammad Umair <181370124@gift.edu.pk>
2022-08-24 10:42:13 +0500

    update Experience

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Intro from "./Components/Intro/intro";
 import Services from "./Components/Services/Services";
+import Experience from "./Components/Experience/Experience";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <Services />
+      <Experience />
     </div>
   );
 }
