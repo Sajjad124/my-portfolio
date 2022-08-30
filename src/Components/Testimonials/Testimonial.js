ca05dc153db8edb1a8209704791298df6af1de36
Muhammad Umair <181370124@gift.edu.pk>
2022-08-30 16:05:32 +0500

    updated

--- src/Components/Testimonials/Testimonial.js
+++ src/Components/Testimonials/Testimonial.js
@@ -1,64 +1,47 @@
 import React from "react";
 import "./Testimonial.css";
-import profile1 from "../../img/profile1.jpg";
-import profile2 from "../../img/profile2.jpg";
-import profile3 from "../../img/profile3.jpg";
-import profile4 from "../../img/profile4.jpg";
-import profile5 from "../../img/profile5.jpg";
+import SideBar from "../../img/sidebar.png";
+import Hoc from "../../img/hoc.png";
+import Ecommerce from "../../img/ecommerce.png";
+import MusicApp from "../../img/musicapp.png";
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
-const clients = [
+const slideImages = [
   {
-    img: profile1,
-    c_reviews:
-      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    url: MusicApp,
+    caption: "Music App",
   },
   {
-    img: profile2,
-    c_reviews:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    url: Ecommerce,
+    caption: "Ecommerce",
   },
   {
-    img: profile3,
-    c_reviews:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    url: Hoc,
+    caption: "Hoc Work",
   },
   {
-    img: profile4,
-    c_reviews:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  },
-  {
-    img: profile5,
-    c_reviews:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    url: SideBar,
+    caption: "Side Bar",
   },
 ];
 
 const Testimonial = () => {
   return (
-    <div className="t-wrap">
-      <div className="t-headings">
-        <span className="head">Testimonial </span>
-        <div
-          className="blur t-blur1"
-          style={{ background: "var(--purple)" }}
-        ></div>
-        <div
-          className="blur t-blur1"
-          style={{ background: "var(--skyblue)" }}
-        ></div>
-        <Slide>
-          {clients.map((client, index) => (
-            <div className="each-slide" key={index}>
-              <img src={client.img} alt="" />
-              <span>{client.c_reviews}</span>
+    <div className="t-slider">
+      <span>Testimonial</span>
+      <Slide>
+        {slideImages.map((slideImage, index) => (
+          <div className="all-slide" key={index}>
+            <img src={slideImage.url} alt="" />
+            <div>
+              dfdfdffffffffffffffffffffffffffffffffffffffff
+              dfdfdffffffffffffffffffffffffffffffffffffffffsdfsfs
             </div>
-          ))}
-        </Slide>
-      </div>
+          </div>
+        ))}
+      </Slide>
     </div>
   );
 };
