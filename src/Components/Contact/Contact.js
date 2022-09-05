951a1bd16bec3706c291b80f1e445c181e1c920c
Muhammad Umair <181370124@gift.edu.pk>
2022-09-06 00:07:36 +0500

    updated contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,10 +4,10 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="c-form">
-      <div className="w-left">
+      <div className="c-left">
         <span>Get in touch</span>
         <span>Contact me</span>
-        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+        <div className="blur c-blur1" style={{ background: "#ABF1FF94" }}></div>
       </div>
       <div className="c-right">
         <form>
@@ -15,20 +15,24 @@ const Contact = () => {
             type="text"
             name="user_name"
             className="user"
-            placeholder="Name"
+            placeholder="Enter your name..."
           ></input>
           <input
             type="email"
             name="user_email"
-            className="email"
-            placeholder="Enter your email"
+            className="user"
+            placeholder="Enter your email..."
           ></input>
           <textarea
             name="message"
             className="message"
-            placeholder="Enter your message"
+            placeholder="enter your message here..."
           ></textarea>
           <input type="submit" value="send" className="button"></input>
+          <div
+            className="blur c-blur2"
+            style={{ background: "var(--purple)" }}
+          ></div>
         </form>
       </div>
     </div>
