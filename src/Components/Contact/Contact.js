769aefc0ec19c165ccb0e567940493fad5a87d1b
Muhammad Umair <181370124@gift.edu.pk>
2022-09-06 23:49:37 +0500

    Email Js

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,29 @@
 import React from "react";
 import "./Contact.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <div className="c-form">
       <div className="c-left">
