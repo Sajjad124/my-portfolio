de830fd51be41fa1603c037eaf7a3dff646ed004
Muhammad Umair <181370124@gift.edu.pk>
2022-09-14 23:50:55 +0500

    iconscout
    iconscout library to use icons

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
 import wave from "../../img/wave.png";
+import insta from "@iconscout/react-unicons/icons/uil-instagram";
 
 const Footer = () => {
   return (
     <div className="footer">
       <img src={wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        
+        <span>SajjadSaroya6@gmail.com</span>
+        <div className="f-icons">
+          <insta />
+        </div>
       </div>
     </div>
   );
