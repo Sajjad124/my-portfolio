fc1d740f8da484de16e8e5e3adc619e0c0366cfb
Muhammad Umair <181370124@gift.edu.pk>
2022-09-15 01:24:12 +0500

    updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,6 +3,7 @@ import wave from "../../img/wave.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
 import Github from "@iconscout/react-unicons/icons/uil-github";
+import "./Footer.css";
 
 const Footer = () => {
   return (
@@ -11,10 +12,9 @@ const Footer = () => {
       <div className="f-content">
         <span>SajjadSaroya6@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size="3rem" />
+          <Insta size="3rem" />
           <Facebook color="white" size="3rem" />
           <Github color="white" size="3rem" />
-          <span>Sajjad</span>
         </div>
       </div>
     </div>
