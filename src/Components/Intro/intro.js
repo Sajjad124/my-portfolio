b6f6352a8fd06d97c6cfee2d68d83ad017fdd1af
Muhammad Umair <181370124@gift.edu.pk>
2022-09-19 23:19:45 +0500

    apply motion

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -12,7 +12,7 @@ import thumbup from "../../img/thumbup.png";
 import Crown from "../../img/crown.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import Boy from "../../img/boy.png";
-import { motion } from "framer-motion";
+import { motion } from "framer-motion/dist/framer-motion";
 
 const intro = () => {
   const transtion = { duration: 2, type: "spring" };
@@ -52,15 +52,26 @@ const intro = () => {
         <motion.img
           initial={{ left: "-36%" }}
           whileInView={{ left: "-24%" }}
+          transtion={transtion}
           src={glassesimoji}
           alt=""
         />
-        <div style={{ top: "-4%", left: "68%" }}>
+        <motion.div
+          initial={{ left: "-74%", top: "-4%" }}
+          whileInView={{ left: "68%" }}
+          transtion={transtion}
+          style={{ top: "-4%", left: "68%" }}
+        >
           <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
-        </div>
-        <div style={{ top: "18rem", left: "0rem" }}>
+        </motion.div>
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transtion={{ transtion }}
+          style={{ top: "18rem", left: "0rem" }}
+        >
           <FloatingDiv image={thumbup} txt1="Best Design" txt2="Award" />
-        </div>
+        </motion.div>
         {/* blur div */}
         <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
         <div
