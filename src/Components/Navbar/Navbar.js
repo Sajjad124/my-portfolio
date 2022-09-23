ad2ba66587a807f0fd923e8ae8b72c23ffd65203
Muhammad Umair <181370124@gift.edu.pk>
2022-09-23 10:55:27 +0500

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Navbar.css";
 import Toggle from "../Toggle/Toggle";
+import { Link } from "react-scroll";
 
 export const Navbar = () => {
   return (
     <div className="n-wrapper">
       <div className="n-left">
         <div className="n-name">Sajjad</div>
-        <Toggle/>
+        <Toggle />
       </div>
       <div className="n-right">
         <div className="n-list">
