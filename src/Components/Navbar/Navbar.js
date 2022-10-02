5b4a6b215d321a969d5230b0a1d9a87dc977da9a
Muhammad Umair <181370124@gift.edu.pk>
2022-10-02 23:56:58 +0500

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -55,7 +55,7 @@ export const Navbar = () => {
               smooth={true}
             >
               {" "}
-              <li>Testimonial</li>
+              <li>Testimonials</li>
             </Link>
           </ul>
         </div>
