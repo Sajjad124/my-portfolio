ae3e21959bc321dcec4a2f8493e43d07a2fa6aa4
Muhammad Umair <181370124@gift.edu.pk>
2022-10-06 10:33:11 +0500

    Update Footer.js

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,6 +2,8 @@ import React from "react";
 import wave from "../../img/wave.png";
 import Insta from "@iconscout/react-unicons/icons/uil-instagram";
 import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Linkedin from "@iconscout/react-unicons/icons/uil-linkedin";
+
 import Github from "@iconscout/react-unicons/icons/uil-github";
 import "./Footer.css";
 
@@ -12,9 +14,21 @@ const Footer = () => {
       <div className="f-content">
         <span>SajjadSaroya6@gmail.com</span>
         <div className="f-icons">
-          <Insta size="3rem" />
-          <Facebook color="white" size="3rem" />
-          <Github color="white" size="3rem" />
+          <a href="https://www.instagram.com/sajjad_saroya/">
+            <Insta size="3rem" />
+          </a>
+          <a href="">
+            <Facebook color="white" size="3rem" />
+          </a>
+          <a title="go to linkedin profile" href="https://github.com/Sajjad124">
+            <Github color="white" size="3rem" />
+          </a>
+          <a
+            title="go to linkedin profile"
+            href="https://pk.linkedin.com/in/sajjad-saroya-1a7359238?trk=people-guest_people_search-card"
+          >
+            <Linkedin color="white" size="3rem" />
+          </a>
         </div>
       </div>
     </div>
